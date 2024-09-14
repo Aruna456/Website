bfcdaa680d6bdfa013b78c84d1103db52724b06c
Aruna456 <aruna.techbox@gmail.com>
2024-09-14 10:33:29 +0530

    Day 5 commited

---
+++ callback.js
@@ -0,0 +1,14 @@
+function success(){
+ 
+    console.log("Success");
+}
+function fail()
+{
+    console.log("Failed");
+    
+}
+function error()
+{
+    console.log("Error");
+    
+}
---
+++ es.js
@@ -0,0 +1,22 @@
+function f1(){
+ 
+     console.log("Hello");
+}
+const data={
+    
+}
+const arrow= () => {
+                
+    for(let i=0;i<data.length;i++)
+    {
+
+    }
+}
+
+
+
+
+
+
+f1();
+arrow();
