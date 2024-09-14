6775f0aa212483488901c71648c745b2d96d48df
Aruna456 <aruna.techbox@gmail.com>
2024-09-14 11:19:28 +0530

    Demo:Used Callback

--- callback.js
+++ callback.js
@@ -12,3 +12,33 @@ function error()
     console.log("Error");
     
 }
+function exe(data,callback){
+
+    if(data)
+    {
+        console.log(`Balance: ${data}`);
+        
+    }
+    setTimeout(callback,3000);
+
+}
+
+function trigger(data){
+
+    if(data>0)
+    {
+        exe(data,success)
+    }
+    else if(data<0)
+    {
+        exe(data,fail)
+    }
+    else 
+    {
+        exe(data,error)
+    }
+
+}
+
+let data=prompt("Enter amount:")
+trigger(data)
